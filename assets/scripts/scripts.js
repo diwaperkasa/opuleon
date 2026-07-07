4bdce46b1d7ace51918042a6ac7d2a4132401f9b
diwaperkasa <diwa.perkasa@gmail.com>
2026-07-07 11:19:41 +0700

    init

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,5 +1,6 @@
 import bootstrap from 'bootstrap';
 import 'sharer.js';
+import Flickity from 'flickity';
 
 const header = document.querySelector('.site-header');
 const fixedHeader = document.querySelector('.site-header-fixed');
@@ -13,4 +14,16 @@ window.addEventListener("scroll", () => {
     } else {
         fixedHeader.classList.remove('show');
     }
+
+    const galleries = document.querySelectorAll('.gallery');
+
+    galleries.forEach((gallery) => {
+        const flkty = new Flickity(gallery, {
+            cellAlign: 'center',
+            freeScroll: false,
+            wrapAround: true,
+            autoPlay: true,
+            pageDots: false
+        });
+    });
 });
