1a087d33476b005654276189835c81f1f289f3b8
diwaperkasa <diwa.perkasa@gmail.com>
2026-07-12 19:27:14 +0700

    init

---
+++ components/newsletter.php
@@ -0,0 +1,15 @@
+<div class="rounded-3 bg-light p-3 sticky-top top-5">
+    <p class="text-warning dm-sans text-uppercase tracking-wide">The Opuleon Letter</p>
+    <p class="h4 playfair-display">One considered read, once a week.</p>
+    <p class="dm-sans">No aggregation. No list for volume. Editorial Picks for reader who pursue the finest things with curiosity.</p>
+    <form>
+        <div class="mb-3">
+            <input type="email" class="rounded form-control dm-sans" id="email" placeholder="Your email address" required />
+        </div>
+        <button type="submit" class="w-100 border-black rounded btn btn-light text-uppercase dm-sans text-warning-hover border-warning-hover">Subscribe
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+            </svg>
+        </button>
+    </form>
+</div>
