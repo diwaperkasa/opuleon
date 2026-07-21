da0e31722c79762b664ecb4d6cda8a4368fca352
diwaperkasa <diwa.perkasa@gmail.com>
2026-07-21 21:52:07 +0700

    update

--- components/footer.php
+++ components/footer.php
@@ -8,7 +8,7 @@
                 <ul class="list-unstyled d-flex gap-3 mb-0">
                     <?php $menus = get_wp_menu_tree('footer') ?>
                     <?php foreach ($menus as $menu): ?>
-                        <li><a href="<?= $menu['url'] ?>" class="text-decoration-none text-dark text-uppercase dm-sans tracking-wide text-warning-hover"><?= $menu['title'] ?></a></li>
+                        <li><a href="<?= $menu['url'] ?>" class="text-decoration-none text-dark text-uppercase dm-sans tracking-wide text-warning-hover d-none d-sm-block"><?= $menu['title'] ?></a></li>
                     <?php endforeach; ?>
                     <li class="d-none d-lg-block"><span class="text-dark text-uppercase dm-sans tracking-wide">&copy; 2026 Opuleon</span></li>
                 </ul>
